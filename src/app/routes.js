77b94e6559d1ccec60c0686c43d39d046e2805c1
Zernalda Septian <zernalda@point-star.com>
2020-01-22 17:35:53 +0700

    delete unfunction routes

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,12 +1,8 @@
 module.exports = function(app) {
     const todo = require('./controller');
     app.route('/').get(todo.index);
-    app.route('/userlist').get(todo.daftarUser);
-    app.route('/extdrivelist').get(todo.daftarExtFile);
-
-    // buat all file admin directory
-    // app.route('/drivelist').get(todo.FileList);
-
+    // app.route('/userlist').get(todo.daftarUser);
+    // app.route('/extdrivelist').get(todo.daftarExtFile);
     app.route('/drivelist').get(todo.FileList);
 
     app.route('/drivepermission/:docid').get(todo.daftarPermissionId);
